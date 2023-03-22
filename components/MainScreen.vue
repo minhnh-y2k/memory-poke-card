0b09f4242d52f3a508e43990efb23ac1d2987b4e
Nguyen Hoai Minh <minhnh.y2k@gmail.com>
2023-03-22 09:28:08 +0700

    Update MainScreen.vue

--- components/MainScreen.vue
+++ components/MainScreen.vue
@@ -1,20 +1,18 @@
 <template>
-  <div class="w-full h-screen flex flex-col justify-center items-center bg-black text-white text-center">
-    <h1 class="text-7xl uppercase font-bold m-0">Memory Poke Card</h1>
-    <p class="text-3xl">Select mode to start game</p>
-    <div class="modes mt-8 flex">
-      <button @click="onStart(4, 4)" class="flex flex-col justify-center items-center w-36 h-36 rounded-3xl border-2 m-4">
+  <div class="screen">
+    <h1>Memory Poke Card</h1>
+    <p>Select mode to start game</p>
+    <div class="modes">
+      <button @click="onStart(4, 4)">
         <span class="text-3xl">4x4</span>
       </button>
-      <button @click="onStart(4, 8)" class="flex flex-col justify-center items-center w-36 h-36 rounded-3xl border-2 m-4">
+      <button @click="onStart(4, 8)">
         <span class="text-3xl">4x8</span>
       </button>
-      <button @click="onStart(5, 10)"
-        class="hidden xl:flex flex-col justify-center items-center w-36 h-36 rounded-3xl border-2 m-4">
+      <button @click="onStart(5, 10)" class="hidden xl:block">
         <span class="text-3xl">5x10</span>
       </button>
-      <button @click="onStart(6, 12)"
-        class="hidden xl:flex flex-col justify-center items-center w-36 h-36 rounded-3xl border-2 m-4">
+      <button @click="onStart(6, 12)" class="hidden xl:block">
         <span class="text-3xl">6x12</span>
       </button>
     </div>
@@ -33,4 +31,48 @@ export default {
 }
 </script>
 
-<style scoped></style>
+<style lang="scss" scoped>
+.screen {
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: black;
+  color: white;
+  text-align: center;
+  h1 {
+    font-size: 5rem;
+    text-transform: uppercase;
+    margin-bottom: 3rem;
+    line-height: 1;
+  }
+  p {
+    font-size: 2rem;
+  }
+  .modes {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+    button {
+      background-color: black;
+      color: white;
+      font-size: 2rem;
+      text-transform: uppercase;
+      border: 2px solid white;
+      cursor: pointer;
+      transition: all 0.3s ease-in-out;
+      width: 6rem;
+      height: 6rem;
+      margin: 1rem;
+      border-radius: 1.5rem;
+      &:hover {
+        background-color: white;
+        color: black;
+      }
+    }
+  }
+}
+</style>
