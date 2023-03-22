d5f0eb97ca3b5df1388a46b70990eeaa899d4e93
Nguyen Hoai Minh <minhnh.y2k@gmail.com>
2023-03-22 15:01:38 +0700

    format time result

--- components/ResultScreen.vue
+++ components/ResultScreen.vue
@@ -1,16 +1,24 @@
 <template>
   <div class="w-full min-h-screen bg-black flex flex-col justify-center items-center text-white">
-    <h1 class="text-8xl font-bold">✨ Congratulations ✨</h1>
-    <p class="text-4xl">Time: {{ timer }} seconds</p>
+    <h1 class="text-6xl font-bold">Congratulations</h1>
+    <p class="text-4xl">Time: {{ formatTime(timer) }}</p>
   </div>
 </template>
 
 <script>
+import moment from 'moment';
+
 export default {
   props: {
     timer: {
       type: Number,
       default: 0,
+    },
+  },
+
+  methods: {
+    formatTime(time) {
+      return moment(time).format('mm:ss');
     },
   },
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 
 const columns = {
+  2: "repeat(2, minmax(0, 1fr))",
   4: "repeat(4, minmax(0, 1fr))",
   8: "repeat(8, minmax(0, 1fr))",
   10: "repeat(10, minmax(0, 1fr))",
