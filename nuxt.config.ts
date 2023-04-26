a7eb86018b599f7bee15da3bef0a96c85d0fcdce
Nguyen Hoai Minh <minhnh.y2k@gmail.com>
2023-04-26 23:28:02 +0700

    add google-site-verification meta

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,14 @@
 export default defineNuxtConfig({
   app: {
     head: {
-      title: 'Memory Poke Card'
+      title: 'Memory Poke Card',
+      meta: [
+        {
+          name: 'google-site-verification',
+          content: 'VXBEISxR68bb7rPEW30GmJTzv4mfhA4lz7XzlHIYG_Y'
+        },
+      ],
+
     }
   },
   css: [
