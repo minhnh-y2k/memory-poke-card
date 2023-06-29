c79a664f878d2649cc090b277f25ab07bf973ae2
Nguyen Hoai Minh <minhnh.y2k@gmail.com>
2023-06-29 07:52:40 +0700

    code improvement

--- components/MainScreen.vue
+++ components/MainScreen.vue
@@ -6,45 +6,35 @@
     </h1>
     <p class="text-3xl">Select mode to start game</p>
     <div class="mt-8 flex">
-      <button @click="onStart(2, 2)"
-        class="m-4 flex h-28 w-28 flex-col items-center justify-center rounded-3xl border-2 transition-all duration-300 ease-in-out hover:bg-white hover:text-black">
+      <button @click="onStart(2, 2)" class="m-4 flex h-28 w-28">
         <span class="text-3xl">2x2</span>
       </button>
 
-      <button @click="onStart(4, 3)"
-        class="m-2 flex h-24 w-24 flex-col items-center justify-center rounded-3xl border-2 transition-all duration-300 ease-in-out hover:bg-white hover:text-black md:hidden">
+      <button @click="onStart(4, 3)" class="m-2 flex h-24 w-24 md:hidden">
         <span class="text-3xl">4x3</span>
       </button>
-      <button @click="onStart(6, 4)"
-        class="m-2 flex h-24 w-24 flex-col items-center justify-center rounded-3xl border-2 transition-all duration-300 ease-in-out hover:bg-white hover:text-black md:hidden">
+      <button @click="onStart(6, 4)" class="m-2 flex h-24 w-24 md:hidden">
         <span class="text-3xl">6x4</span>
       </button>
-      <button @click="onStart(7, 4)"
-        class="m-2 flex h-24 w-24 flex-col items-center justify-center rounded-3xl border-2 transition-all duration-300 ease-in-out hover:bg-white hover:text-black md:hidden">
+      <button @click="onStart(7, 4)" class="m-2 flex h-24 w-24 md:hidden">
         <span class="text-3xl">7x4</span>
       </button>
-      <button @click="onStart(4, 4)"
-        class="m-4 hidden h-28 w-28 flex-col items-center justify-center rounded-3xl border-2 transition-all duration-300 ease-in-out hover:bg-white hover:text-black md:flex">
+      <button @click="onStart(4, 4)" class="m-4 hidden h-28 w-28 md:flex">
         <span class="text-3xl">4x4</span>
       </button>
-      <button @click="onStart(6, 5)"
-        class="m-4 hidden h-28 w-28 flex-col items-center justify-center rounded-3xl border-2 transition-all duration-300 ease-in-out hover:bg-white hover:text-black md:flex xl:hidden">
+      <button @click="onStart(6, 5)" class="m-4 hidden h-28 w-28 md:flex xl:hidden">
         <span class="text-3xl">6x5</span>
       </button>
-      <button @click="onStart(8, 6)"
-        class="m-4 hidden h-28 w-28 flex-col items-center justify-center rounded-3xl border-2 transition-all duration-300 ease-in-out hover:bg-white hover:text-black md:flex xl:hidden">
+      <button @click="onStart(8, 6)" class="m-4 hidden h-28 w-28 md:flex xl:hidden">
         <span class="text-3xl">8x6</span>
       </button>
-      <button @click="onStart(4, 8)"
-        class="m-4 hidden h-28 w-28 flex-col items-center justify-center rounded-3xl border-2 transition-all duration-300 ease-in-out hover:bg-white hover:text-black xl:flex">
+      <button @click="onStart(4, 8)" class="m-4 hidden h-28 w-28 xl:flex">
         <span class="text-3xl">4x8</span>
       </button>
-      <button @click="onStart(5, 10)"
-        class="m-4 hidden h-28 w-28 flex-col items-center justify-center rounded-3xl border-2 transition-all duration-300 ease-in-out hover:bg-white hover:text-black xl:flex">
+      <button @click="onStart(5, 10)" class="m-4 hidden h-28 w-28 xl:flex">
         <span class="text-3xl">5x10</span>
       </button>
-      <button @click="onStart(6, 12)"
-        class="m-4 hidden h-28 w-28 flex-col items-center justify-center rounded-3xl border-2 transition-all duration-300 ease-in-out hover:bg-white hover:text-black xl:flex">
+      <button @click="onStart(6, 12)" class="m-4 hidden h-28 w-28 xl:flex">
         <span class="text-3xl">6x12</span>
       </button>
     </div>
@@ -81,5 +71,13 @@ const onStart = (rows, cols) => {
   background-position: top;
   background-attachment: scroll;
   background-size: cover;
+
+  button {
+    @apply items-center justify-center rounded-3xl border-2 transition-all duration-300 ease-in-out;
+
+    &:hover {
+      @apply bg-white text-black;
+    }
+  }
 }
 </style>
